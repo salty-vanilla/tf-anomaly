9f633ef7e43f421cd0db25bb7077a9ec5e849563
salty_vanilla <jacketgardensio@gmail.com>
2018-10-27 12:17:37 +0900

    create GMMLayer to avoid NotImplementedError

--- impl/dagmm/gmm.py
+++ impl/dagmm/gmm.py
@@ -2,37 +2,35 @@ import tensorflow as tf
 import numpy as np
 
 
-class GMM(tf.keras.Model):
+class GMMLayer(tf.keras.layers.Layer):
     def __init__(self, nb_components,
-                 nb_features):
+                 nb_features,
+                 eps=1e-6):
         super().__init__()
 
         self.nb_components = nb_components
         self.nb_features = nb_features
+        self.eps = eps
 
         self.phi = self.add_variable('phi',
-                                     (nb_components, ),
+                                     (nb_components,),
                                      dtype=tf.float32,
-                                     initializer=tf.keras.initializers.zeros(),
-                                     trainable=False)
+                                     initializer=tf.keras.initializers.Zeros())
 
         self.mu = self.add_variable('mu',
                                     (nb_components, nb_features),
                                     dtype=tf.float32,
-                                    initializer=tf.keras.initializers.zeros(),
-                                    trainable=False)
+                                    initializer=tf.keras.initializers.Zeros())
 
         self.sigma = self.add_variable('sigma',
                                        (nb_components, nb_features, nb_features),
                                        dtype=tf.float32,
-                                       initializer=tf.keras.initializers.zeros(),
-                                       trainable=False)
+                                       initializer=tf.keras.initializers.Zeros())
 
         self.L = self.add_variable('L',
                                    (nb_components, nb_features, nb_features),
                                    dtype=tf.float32,
-                                   initializer=tf.keras.initializers.zeros(),
-                                   trainable=False)
+                                   initializer=tf.keras.initializers.Zeros())
 
     def call(self, inputs,
              training=None,
@@ -46,25 +44,25 @@ class GMM(tf.keras.Model):
             # k   : index of components
             # l,m : index of features
             gamma_sum = tf.reduce_sum(gamma, axis=0)
-            self.phi = tf.reduce_mean(gamma, axis=0)
-            self.mu = tf.einsum('ik,il->kl', gamma, z) / gamma_sum[:, None]
+            tf.assign(self.phi, tf.reduce_sum(gamma, axis=0))
+            tf.assign(self.mu, tf.einsum('ik,il->kl', gamma, z) / gamma_sum[:, None])
             z_centered = tf.sqrt(gamma[:, :, None]) * (z[:, None, :] - self.mu[None, :, :])
-            self.sigma = tf.einsum('ikl,ikm->klm',
-                                   z_centered,
-                                   z_centered)
-            self.sigma /= gamma_sum[:, None, None]
-
+            sigma = tf.einsum('ikl,ikm->klm',
+                              z_centered,
+                              z_centered)
+            sigma /= gamma_sum[:, None, None]
+            tf.assign(self.sigma, sigma)
             # Calculate a cholesky decomposition of covariance in advance
             n_features = z.shape[1]
-            min_vals = tf.diag(tf.ones(n_features, dtype=tf.float32)) * 1e-6
-            self.L = tf.cholesky(self._sigma + min_vals[None, :, :])
+            min_vals = tf.diag(tf.ones(n_features, dtype=tf.float32)) * self.eps
+            tf.assign(self.L, tf.cholesky(self.sigma + min_vals[None, :, :]))
 
         z_centered = z[:, None, :] - self.mu[None, :, :]  # ikl
         v = tf.matrix_triangular_solve(self.L,
                                        tf.transpose(z_centered, [1, 2, 0]))  # kli
 
         # log(det(Sigma)) = 2 * sum[log(diag(L))]
-        log_det_sigma = 2.0 * tf.reduce_sum(tf.log(tf.matrix_diag_part(L)), axis=1)
+        log_det_sigma = 2.0 * tf.reduce_sum(tf.log(tf.matrix_diag_part(self.L)), axis=1)
 
         # To calculate energies, use "log-sum-exp" (different from orginal paper)
         d = z.get_shape().as_list()[1]
@@ -75,5 +73,24 @@ class GMM(tf.keras.Model):
 
         return energies
 
+
+class GMM(tf.keras.Model):
+    def __init__(self, nb_components,
+                 nb_features):
+        super().__init__()
+
+        self.nb_components = nb_components
+        self.nb_features = nb_features
+
+        self.gmm_layer = GMMLayer(nb_components,
+                                  nb_features)
+
+    def call(self, inputs,
+             training=None,
+             mask=None):
+        return self.gmm_layer(inputs,
+                              training,
+                              mask)
+
     def cov_diag_loss(self):
-        return tf.reduce_sum(tf.divide(1, tf.matrix_diag_part(self.sigma)))
+        return tf.reduce_sum(tf.divide(1, tf.matrix_diag_part(self.gmm_layer.sigma)))
