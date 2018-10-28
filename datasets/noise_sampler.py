53f596f120af22743b04d2a69d2e4cb2251cb8fd
Nakatsuka Shunsuke <nakatsuka@cv.info.gifu-u.ac.jp>
2018-10-28 19:14:40 +0900

    add seed

--- datasets/noise_sampler.py
+++ datasets/noise_sampler.py
@@ -2,11 +2,16 @@ import numpy as np
 
 
 class NoiseSampler:
-    def __init__(self, mode='uniform'):
+    def __init__(self, mode='uniform',
+                 seed=None):
         self.mode = mode
+        self.seed = seed
 
     def __call__(self, batch_size, noise_dim):
+        if self.seed is not None:
+            np.random.seed(seed=self.seed)
         if self.mode == 'uniform':
-            return np.random.uniform(-1., 1., size=(batch_size, noise_dim))
+            return np.random.uniform(-1., 1.,
+                                     size=(batch_size, noise_dim))
         elif self.mode == 'normal':
             return np.random.normal(0., 1., size=(batch_size, noise_dim))
