09de8ba6dffdb33b1fa4d3a31b37e7826255a295
salty-vanilla <jacketgardensio@gmail.com>
2020-03-13 15:49:39 +0000

    [Update] debugged

--- impl/deep-svdd/mnist/model.py
+++ impl/deep-svdd/mnist/model.py
@@ -19,12 +19,17 @@ class DeepSVDD(_DeepSVDD):
 
         self.convs = [ConvBlock(nb_filter*(2**i),
                                 kernel_size=(5, 5),
+                                kernel_regularizer=tf.keras.regularizers.l2(1.),
                                 **self.conv_block_params)
                       for i in range(3)]
         self.dense1 = DenseBlock(128,
                                  activation_='lrelu',
+                                 kernel_regularizer=tf.keras.regularizers.l2(1.),
+                                 use_bias=False,
                                  normalization=self.normalization)
-        self.dense2 = DenseBlock(self.output_dim)
+        self.dense2 = DenseBlock(self.output_dim,
+                                 kernel_regularizer=tf.keras.regularizers.l2(1.),
+                                 use_bias=False)
 
     def call(self, inputs,
              training=None):
--- impl/deep-svdd/solver.py
+++ impl/deep-svdd/solver.py
@@ -20,14 +20,13 @@ class Solver(object):
         self.nu = nu
         self.opt = tf.keras.optimizers.Adam(lr, beta_1=0.5, beta_2=0.9)
         self.logdir = logdir
-        self.center = tf.Variable()
-        self.radius = tf.Variable()
+        self.center = tf.Variable(np.zeros((self.model.output_dim)),
+                                  dtype=tf.float32)
+        self.radius = tf.Variable(0., dtype=tf.float32)
         self.checkpoint = tf.train.Checkpoint(center=self.center,
                                               radius=self.radius)
 
     def update(self, x):
-        weights = [layer.kernel for layer in self.model.layers
-                   if hasattr(layer, 'kernel')]
         with tf.GradientTape() as tape:
             y = self.model(x, training=True)
             distances = tf.reduce_sum((y-self.center)**2, axis=-1)
@@ -35,15 +34,14 @@ class Solver(object):
             if self.objective == 'one-class':
                 d = tf.reduce_mean(distances)
             else:
-                distances -= self.radius**2
+                _distances = distances - self.radius**2
                 d = self.radius**2 \
                     + (1./self.nu)*tf.reduce_mean(tf.math.maximum(
-                                                    tf.zeros_like(distances), 
-                                                    distances))
+                                                    tf.zeros_like(_distances), 
+                                                    _distances))
 
-            l2_w = tf.reduce_mean([tf.nn.l2_loss(w) for w in weights])
-            
-            loss = d + (self.lambda_/2.)*l2_w
+            l2_loss = tf.reduce_sum([l.losses[0] for l in self.model.layers])
+            loss = d + (self.lambda_/2.)*l2_loss
         grads = tape.gradient(loss, self.model.trainable_variables)
         self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
         return loss, distances
@@ -51,16 +49,19 @@ class Solver(object):
     def fit(self, x,
             batch_size=64,
             nb_epoch=100,
+            warm_up_epoch=10,
             save_steps=10):
         image_sampler = ImageSampler(normalize_mode='tanh').flow(x,
                                                                  y=None,
                                                                  batch_size=batch_size)
         self.fit_generator(image_sampler,
                            nb_epoch=nb_epoch,
+                           warm_up_epoch=warm_up_epoch,
                            save_steps=save_steps)
 
     def fit_generator(self, image_sampler,
                       nb_epoch=100,
+                      warm_up_epoch=10,
                       save_steps=10):
         batch_size = image_sampler.batch_size
         nb_sample = image_sampler.nb_sample
@@ -74,10 +75,6 @@ class Solver(object):
 
         for epoch in range(1, nb_epoch + 1):
             print('\nepoch {} / {}'.format(epoch, nb_epoch))
-
-            if self.objective == 'soft-boundary':
-                self.radius = self.compute_radius
-
             start = time.time()
 
             for iter_ in range(1, steps_per_epoch + 1):
@@ -92,18 +89,18 @@ class Solver(object):
                       f'loss : {loss:.4f}',
                       end='\r')
                 
-                self.compute_radius(disances)
+                if epoch > warm_up_epoch:
+                    self.compute_radius(disances)
             
             if epoch % save_steps == 0:
                 os.makedirs(os.path.join(self.logdir, 'model'), exist_ok=True)
                 self.model.save_weights(os.path.join(self.logdir, 'model', 'model_%d' % epoch))
-                self.checkpoint.save(os.path.join(self.logdir, 'model', 'center_radius' % epoch))
+                self.checkpoint.save(os.path.join(self.logdir, 'model', 'center_radius_%d' % epoch))
 
     def compute_center(self, image_sampler, 
                        steps_per_epoch):
         print('Calculating Center ... ', end='')
-        output_dim = self.model.output.shape[-1]
-        y = tf.raw_ops.Empty(shape=(0, output_dim), dtype=tf.float32)
+        y = tf.raw_ops.Empty(shape=(0, self.model.output_dim), dtype=tf.float32)
 
         for _ in range(1, steps_per_epoch + 1):
             x = image_sampler()
