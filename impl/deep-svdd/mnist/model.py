ea1f95f040063952b200150edeac1fda9a8200c2
salty-vanilla <jacketgardensio@gmail.com>
2020-03-22 07:20:53 +0000

    [Update] add norm_params

--- impl/deep-svdd/mnist/model.py
+++ impl/deep-svdd/mnist/model.py
@@ -17,15 +17,20 @@ class DeepSVDD(_DeepSVDD):
                          normalization,
                          downsampling)
 
+        norm_params = {'center': False,
+                       'scale': False}
+
         self.convs = [ConvBlock(nb_filter*(2**i),
                                 kernel_size=(5, 5),
                                 kernel_regularizer=tf.keras.regularizers.l2(1.),
+                                norm_params=norm_params,
                                 **self.conv_block_params)
                       for i in range(3)]
         self.dense1 = DenseBlock(128,
                                  activation_='lrelu',
                                  kernel_regularizer=tf.keras.regularizers.l2(1.),
                                  use_bias=False,
+                                 norm_params=norm_params,
                                  normalization=self.normalization)
         self.dense2 = DenseBlock(self.output_dim,
                                  kernel_regularizer=tf.keras.regularizers.l2(1.),
